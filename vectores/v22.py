4de02e920be50c8e3e88e2b06c788b0a715e14f6
LucasLeone <lucasagustinleone2003@gmail.com>
2021-06-07 16:51:39 -0300

    v22 finished

--- vectores/v22.py
+++ vectores/v22.py
@@ -4,3 +4,21 @@
     debe ser ordenado mediante el método de la burbuja. Una vez ordenado, debe ser escrito
     por pantalla.
 '''
+
+vector = []
+
+print('---- Llenado de la lista de notas ----')
+for i in range(0, 20):
+    num = float(input('Ingrese la nota: '))
+    vector.append(num)
+
+print('---- Ordenamiento burbuja ----')
+for i in range(1, len(vector)):
+    for j in range(0, len(vector) - i):
+        if (vector[j+1] <  vector[j]):
+            aux = vector[j]
+            vector[j] = vector[j+1]
+            vector[j+1] = aux
+
+print(f'El vector ordenado es:')
+print(vector)
---
+++ vectores/v23.py
@@ -0,0 +1,20 @@
+'''
+    Dos vectores de 100 elementos contienen los pesos y las alturas de un grupo de
+    alumnos. Un tercer vector contiene el sexo de los alumnos (0 si es hombre, 1 si es
+    mujer). Los datos correspondientes a un alumno se encuentran en la mima posición en
+    los tres vectores. Escribir un programa que:
+        Lea los datos de los 100 alumnos.
+        Encuentre el alumno más alto y la alumna más alta.
+        Muestre todos los alumnos que midan más de 180cm y pesen menos de 80k.
+        Decir si se puede hacer un equipo de baloncesto masculino con estos alumnos (5
+        jugadores de más de 190cm).
+        Decir si se puede hacer un equipo de equitación femenino (10 alumnas de menos de 50k
+        y menos de 160cm).
+        Liste todos los alumnos. Indicar: código de alumno, peso, altura y sexo (‘H’ si es hombre
+        y ‘M’ si es mujer).
+        Liste todas las alumnas de mayor a menor altura.
+        Liste todos los alumnos de menor a mayor peso.
+        Solicitar los datos de un alumno (sexo, peso y altura) y mostrar:
+        El primer alumno que cumpla las condiciones.
+        El último alumno que cumpla las condiciones.
+'''
