4f111dcd03effd7563948083d08cf4fc779f030b
Lucas Leone <lucasleonetatto@gmail.com>
2021-06-07 16:25:55 -0300

    v22 init

---
+++ vectores/v22.py
@@ -0,0 +1,6 @@
+'''
+    Escribir un programa que declare un vector de 20 números reales, correspondientes a
+    las notas de un grupo de alumnos. El vector debe ser leído por teclado, y posteriormente
+    debe ser ordenado mediante el método de la burbuja. Una vez ordenado, debe ser escrito
+    por pantalla.
+'''
